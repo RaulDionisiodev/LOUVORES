94348b4c8bb468c527fea2f58451704e84b429ba
Raul Dionisio <rauldionisiosh@gmail.com>
2021-03-28 12:45:12 -0300

    First commit

---
+++ src/main/java/com/rauldionisio/louvores/LouvoresApplication.java
@@ -0,0 +1,13 @@
+package com.rauldionisio.louvores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LouvoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LouvoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rauldionisio/louvores/LouvoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rauldionisio.louvores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LouvoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
